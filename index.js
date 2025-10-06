93d9b8b9e8c82ef6d1bc15b8c0c805ea212d45b9
bootcamp-rc <kookliztz10@gmail.com>
2025-10-06 08:48:28 +0000

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from housing-tst on Heroku!</h1>');
-});
-
-app.listen(PORT, () => console.log('Server running on port', PORT));
